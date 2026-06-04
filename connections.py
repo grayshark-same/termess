bbf381e1e8fa849deebc1faab328b0238fc58fec
grayshark_ <basov2764@gmail.com>
2026-06-04 16:06:46 +1100

    encryption added

--- connections.py
+++ connections.py
@@ -6,7 +6,7 @@ from prompt_toolkit import PromptSession
 from prompt_toolkit.patch_stdout import patch_stdout
 from storage import *
 
-async def exchange_keys(ws, my_priv_key_b64):
+async def exchange_keys(ws):
     # загрузить свой приватный ключ
     my_pub_key, my_priv_key = get_keys()
 
@@ -15,27 +15,29 @@ async def exchange_keys(ws, my_priv_key_b64):
     their_pub_key = PublicKey(base64.b64decode(their_pub_key_b64))
     return Box(my_priv_key, their_pub_key)
 
-async def chat(ws, username):
+async def chat(ws, username, box):
     session = PromptSession()
     stop = asyncio.Event()
 
     async def receive():
         try:
             async for msg in ws:
-                print(f"{username}: {msg}")
+                decrypted = box.decrypt(base64.b64decode(msg)).decode()
+                print(f"{username}: {decrypted}")
         except websockets.exceptions.ConnectionClosedError:
             print(f"\n{username} disconnected")
 
     async def send():
         with patch_stdout():
             try:
-                await ws.send(get_keys()[0])
+                # await ws.send(get_keys()[0])
                 while True:
                     text = await session.prompt_async("> ")
                     if text.startswith('/quit'):
                         stop.set()
                         return
-                    await ws.send(text)
+                    encrypted = base64.b64encode(box.encrypt(text.encode())).decode()
+                    await ws.send(encrypted)
             except KeyboardInterrupt:
                 stop.set()
     await asyncio.gather(receive(), send())
@@ -45,7 +47,7 @@ async def listen(port, username):
     async def handler(ws):
         box = await exchange_keys(ws)
         print(f"connected")
-        await chat(ws, username)
+        await chat(ws, username, box)
     async with websockets.serve(handler, "0.0.0.0", port):
         # print(f"listening port {port}...")
         await asyncio.Future()
@@ -56,7 +58,7 @@ async def connect(host, port, username):
         async with websockets.connect(f"ws://{host}:{port}") as ws:
             box = await exchange_keys(ws)
             print("connected")
-            await chat(ws, username)
+            await chat(ws, username, box)
     except (ConnectionRefusedError, TimeoutError, OSError):
         print("waiting for connecting")
         await listen(port, username)
